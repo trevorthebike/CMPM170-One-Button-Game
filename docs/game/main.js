daa53e836d4a085a295717f99b95ba7e3b957ede
Trevor Gardner <trevorthebike@gmail.com>
2023-11-17 16:48:34 -0800

    seems finished

--- docs/game/main.js
+++ docs/game/main.js
@@ -1,12 +1,8 @@
 title = "MOVEMENT GAME";
 
 description = `
-    [TAP] 
-  change direction
-
-  avoid green
-  collect blue
-  get to yellow
+    [TAP] to shoot
+    Defend your castle
 `;
 
 characters = [
@@ -21,13 +17,14 @@ characters = [
   l  l
  l ll l
   l  l
- `,
-  `
- 
-  lll  
-    llll
-`
-,
+ `,, `
+
+
+
+ l  l
+l ll l
+ l  l
+`,
 
 ];
 
@@ -37,7 +34,7 @@ options = {
   isReplayEnabled: true,
   isCapturing: true, 
 };
-let cutter;
+let cannon;
 let county = 90;
 let countx = 60;
 let velocity = vec(0, 0);
@@ -51,8 +48,9 @@ let speedblock = 0.2;
 let positiony = 0;
 let center;
 let shot;
-let enemies = [];
-let rects = [];
+let newrect;
+let timer = 0;
+let actual_timer=0
 
 function changedirection(){
   const velocities = [vec(0, 1), vec(0, -1)];
@@ -60,49 +58,45 @@ function changedirection(){
   directionIndex = (directionIndex + 1) % velocities.length;
 }
 
-function generateCuts(centerX, centerY, radius) {
-  let angle = cutter.angle; // Use the cutter's current angle
-  let endPoint = getPoint(radius, angle); // Get the endpoint based on the radius and angle
+//credit to my teammates for this function
+function generateShots(centerX, centerY, radius) {
+  let angle = cannon.angle;
+  let endPoint = getPoint(radius, angle);
   color("red");
   shot = line(centerX, centerY, centerX + endPoint.x, centerY + endPoint.y, 1);
 }
 
 function getPoint(radius, angle) {
-  // Calculate the x and y coordinates based on the radius and angle
   let x = radius * Math.cos(angle);
   let y = radius * Math.sin(angle);
   return { x: x, y: y };
 }
 
-
-function moveCutter() {
-  // Radius of outer edge is 40, angle spans from -pi to pi 
-  cutter.pos.x = center.pos.x + 0.1 * Math.cos(cutter.angle);
-  cutter.pos.y = center.pos.y + 0.1 * Math.sin(cutter.angle);
-  
-  cutter.angle += cutter.speed;
-  // Added a speed modifier and an angle offset to the cutter's rotation, I have no idea why this works but it actually works
-  cutter.rotation = (cutter.pos.angleTo(center.pos) * 0.65) + 1;
-
-  if (cutter.angle > PI/2) {
-    cutter.angle = PI/2;
-    cutter.speed = -cutter.speed; // Reverse direction
-  } else if (cutter.angle < -PI/2) {
-    cutter.angle = -PI/2;
-    cutter.speed = -cutter.speed; // Reverse direction
+//credit to my teammates for this function
+function movecannon() {
+  cannon.pos.x = center.pos.x + 0.1 * Math.cos(cannon.angle);
+  cannon.pos.y = center.pos.y + 0.1 * Math.sin(cannon.angle);
+  cannon.angle += cannon.speed;
+  cannon.rotation = (cannon.pos.angleTo(center.pos) * 0.65) + 1;
+  if (cannon.angle > PI/2) {
+    cannon.angle = PI/2;
+    cannon.speed = -cannon.speed;
+  } else if (cannon.angle < -PI/2) {
+    cannon.angle = -PI/2;
+    cannon.speed = -cannon.speed; 
   }
 }
 
 function createBlocks(){
   console.log("createBlocks");
-  speedblock = speedblock * 1.0001;
-  cutter.speed = cutter.speed * 1.0001;
+  speedblock = speedblock * 1.0002;
+  cannon.speed = cannon.speed * 1.0002;
   if(x1>10){
     x1 -= 1*speedblock;
   }
   if(x1<=10){
     x1 = 90;
-    y1 = rndi(0,90);
+    y1 = rndi(10,80);
     score++;
   }
   if(x2>10){
@@ -110,58 +104,63 @@ function createBlocks(){
   }
   if(x2<=10){
     x2 = 90;
-    y2 = rndi(0,90);
+    y2 = rndi(10,80);
     score++;
   }
-  //let enemey1 = char("b", x1, y1);
-  //let enemy2 = char("b", x2, y2);\
-  
   color("blue");
-  let newrect = rect(x1, y1, 7, 5);
+  char("b", x1, y1);
   color("yellow");
-  let newrect1 = rect(x2, y2, 7, 5);
+  char("c", x2, y2);
 }
-
 
 function createMap(){
   color("red");
   let top = rect(0, 98, 100, 2)
   let bottom = rect(0, 0, 100, 2);
-  let stand = rect(0, 100, 10, -50);
+  let stand = rect(0, 100, 8, -100);
+  color("black");
+  text(`Timer: ${timer}`,  15, 2, {
+    scale: { x: 1, y: 1 }
+  });
 }
 
 function update() {
   if (!ticks) {
-    cutter = { pos: vec(50, 5), angle: 0, rotation: 0, speed: 0.02 };
+    cannon = { pos: vec(50, 5), angle: 0, rotation: 0, speed: 0.03 };
     center = { pos: vec(10, 50) };
     score = 0;
   }
   createMap();
   color("light_black");
-  char("a", cutter.pos, {rotation: cutter.rotation});
-  moveCutter();
-  let lineLength = 10; // Adjust the length as needed
-  let lineEnd = vec(cutter.pos.x + lineLength * Math.cos(cutter.angle),
-                    cutter.pos.y + lineLength * Math.sin(cutter.angle));
-  color("cyan"); // Choose an appropriate color for the line
-  line(cutter.pos.x, cutter.pos.y, lineEnd.x, lineEnd.y, 2);
+  char("a", cannon.pos, {rotation: cannon.rotation});
+  movecannon();
+  let lineLength = 10;
+  let lineEnd = vec(cannon.pos.x + lineLength * Math.cos(cannon.angle),
+                    cannon.pos.y + lineLength * Math.sin(cannon.angle));
+  color("cyan");
+  line(cannon.pos.x, cannon.pos.y, lineEnd.x, lineEnd.y, 2);
   createBlocks();
   county += velocity.y*playerspeed/2;
   countx = clamp(countx, 0, 99);
   county = clamp(county, 0, 99);
   if(input.isJustPressed){
     let mapRadius = 100; 
-    generateCuts(center.pos.x, center.pos.y, mapRadius);
-    if(shot.isColliding.rect.yellow){
-      score--;
+    generateShots(center.pos.x, center.pos.y, mapRadius);
+    if(shot.isColliding.char.c){
+      x2 = 90;
     } 
-    if(shot.isColliding.rect.blue){
-      score-= 2;
+    if(shot.isColliding.char.b){
+      x1 = 90;
     } 
   }
-  if(score >= 50){
-    // /score = 0;
+  if(score >= 10){
     end();
     score = 0;
+    timer = 0;
+  }
+  actual_timer++;
+  if(actual_timer==60){
+    actual_timer = 0;
+    timer++;
   }
 }
